7dc9d6622bc6f24b27cd2efa48b6ec6444c2db01
GalanBogdan <75744101+GalanBogdan@users.noreply.github.com>
2020-12-09 15:55:00 +0000

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import UniversityCourseStudent from './UniversityCourseStudent';
+import UniversityCourseModulePanel from './UniversityCourseModulePanel';
+import Modules from './Modules';
+
+//task 1, part 1 and part 2 (i think) assessment 2.
+
+function App() {
+  return (
+    <div>
+      <UniversityCourseStudent name="Bogdan" studentId = "21381698" courseName= "Web Developing" courseStartDate="14/10/2020"/>
+      <UniversityCourseModulePanel   />
+      <Modules/> 
+    </div>
+  );
+}
+
+export default App;
