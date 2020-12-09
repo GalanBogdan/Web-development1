f289392b10ac0243ee65cc07085bd8d20a1851e0
GalanBogdan <75744101+GalanBogdan@users.noreply.github.com>
2020-12-09 15:53:10 +0000

    Create UniversityCourseModulePane.js

---
+++ UniversityCourseModulePane.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+//task 2 
+function UniversityCourseModulePanel () {
+
+  const [moduleName, setModuleName, moduleCode, setModuleCode, setEndDate, endDate, courseName, setCourseName ] = React.useState(''); 
+
+  const list = [moduleName, moduleCode, endDate,courseName];//AICI NU S TIU SIGUR DACA E BINE
+
+  
+
+  function clickHandler () {
+
+  //create a json, PRIMESC EROARE CA E DETERMINATA DAR NU FOLOSITA
+    const jsonvalue= {
+          'moduleCode':moduleCode,
+          'moduleName': moduleName,
+          'endDate': endDate, 
+          'courseName':courseName
+          
+    }
+
+  
+
+    window.alert("Module Name =  " + moduleName);
+  }
+/
+  return (
+    
+    <div>
+        
+     <input  type="text" placeholder="module name" value={moduleName}  onChange= { e => setModuleName(e.target.value) } ></input>
+     <input  type="text" placeholder="module code" value={moduleCode}  onChange= { e => setModuleCode(e.target.value) } ></input>
+     <input  type="text" placeholder="end date" value={endDate}  onChange= { e => setEndDate(e.target.value) } ></input>
+     <input  type="text" placeholder="course name" value={courseName}  onChange= { e => setCourseName(e.target.value) } ></input>
+
+
+      <button onclick = {clickHandler} >Show</button>
+    </div>
+  );//part 2 and 3 
+
+
+}
+
+export default UniversityCourseModulePanel;
